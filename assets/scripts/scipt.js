95359747d6e62df12ce0134f842301a747abf8c4
[Kemal Becarevic] <[kemal@becarevic.net]>
2022-11-06 20:41:02 +0100

    git

--- assets/scripts/scipt.js
+++ assets/scripts/scipt.js
@@ -3,22 +3,45 @@ const buttons = Array.from(buttonList.querySelectorAll("li"));
 const colorBox = document.getElementById("color-box");
 
 let currentColor = null;
+let correctButton = null;
 
 const generateNewColor = () => {
     const red = Math.floor(Math.random() * 256);
     const green = Math.floor(Math.random() * 256);
     const blue = Math.floor(Math.random() * 256);
-    const color = {
+    return {
         red: red,
         green: green,
         blue: blue,
     };
-    return color;
 };
 
 const setUpColor = () => {
     currentColor = generateNewColor();
-    colorBox.style.backgroundColor = `rgb(${currentColor.red}, ${currentColor.green}, ${currentColor.blue})`
+    correctButton = Math.floor(Math.random() * 3);
+    buttons.forEach((button, index, buttons) => {
+        if(index === correctButton){
+            button.textContent = `rgb (${currentColor.red}, ${currentColor.green}, ${currentColor.blue})`;
+        }else{
+            const rndmColor = generateNewColor();
+            button.textContent = `rgb (${rndmColor.red}, ${rndmColor.green}, ${rndmColor.blue})`
+        }
+    });
+    colorBox.style.backgroundColor = `rgb(${currentColor.red}, ${currentColor.green}, ${currentColor.blue})`;
 };
 
-setUpColor()
+const buttonClickHandler = (buttonIndex) => {
+    console.log(buttonIndex, correctButton);
+    if(buttonIndex === correctButton){
+        console.log("Correct!");
+        setUpColor();
+    }else{
+        console.log("Not correct!");
+    }
+}
+
+setUpColor();
+
+buttons.forEach((button, index, buttons) => {
+    button.addEventListener('click', buttonClickHandler.bind(this, index));
+})
